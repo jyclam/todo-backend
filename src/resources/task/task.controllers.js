268336846af6e90a9d97407aa327cc597642c3f7
John <john.dev.work.01@gmail.com>
2020-08-31 21:52:52 -0700

    Add Task createOne override

--- src/resources/task/task.controllers.js
+++ src/resources/task/task.controllers.js
@@ -1,4 +1,22 @@
 import { crudControllers } from "../../utils/crud";
 import { Task } from "./task.model";
 
-export default crudControllers(Task);
+const createOne = async (req, res) => {
+  const createdBy = req.user._id;
+  const listId = req.body.listId;
+
+  if (!listId) return res.status(400).json({ error: "ListId required." });
+
+  try {
+    const doc = await Task.create({ ...req.body, createdBy, list: listId });
+    res.status(201).json({ data: doc });
+  } catch (e) {
+    console.error(e);
+    res.status(400).end();
+  }
+};
+
+export default {
+  ...crudControllers(Task),
+  createOne,
+};
